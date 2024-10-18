2ee08b3f14b45c2f1ddd800a916b93a27ca13780
Greeshma Ann Mathew <greeshma.mathew@gmail.com>
2024-10-18 09:55:32 +0200

    modified test case for add task function

--- main_test.go
+++ main_test.go
@@ -7,13 +7,14 @@ import (
 )
 
 func TestAddTask(t *testing.T) {
-	t.Run("Successfully adds task", func(t *testing.T) {
+	t.Run("Successfully adds task 'Task1'", func(t *testing.T) {
 		data := []task{}
 		var id int
 		var err error
 		data, id, err = addTask(data, "Task1")
 		require.Nil(t, err)
-		require.NotEqual(t, 0, id)
-		require.NotEqual(t, 0, len(data))
+		require.Equal(t, 1, id)
+		require.Equal(t, "Task1", data[0].Description)
+		require.Equal(t, StatusToDo, data[0].Status)
 	})
 }
